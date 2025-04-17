927f617f4fa063776083a087454a5c77dce101a4
Narendran Rangarajan <145794141+NarendranR98@users.noreply.github.com>
2025-04-17 08:19:28 -0400

    Add files via upload

---
+++ nrangar_p1/ece748_projects/project_benches/decode/sim/filelist.f
@@ -0,0 +1,9 @@
+../rtl/verilog/data_defs.v
+../rtl/verilog/Decode_Pipelined.v
+../../../verification_ip/interface_packages/decode_in_pkg/decode_in_pkg.sv
+../../../verification_ip/interface_packages/decode_in_pkg/src/decode_in_if.sv
+../../../verification_ip/interface_packages/decode_in_pkg/src/decode_in_driver_bfm.sv
+../../../verification_ip/interface_packages/decode_in_pkg/src/decode_in_monitor_bfm.sv
+../tb/tests/decode_test_pkg/decode_test_pkg.sv
+../tb/testbench/hdl_top.sv
+../tb/testbench/hvl_top.sv
